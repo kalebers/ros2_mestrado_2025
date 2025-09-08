368ae4e553637294a0b46c89e3a20ee1100a0584
kalebers <windks@outlook.com>
2025-09-08 02:49:04 +0200

    fix para go_to_goal e draw_square

--- build/turtle_sim_pkg/build/lib/turtle_sim_pkg/draw_sqaure.py
+++ build/turtle_sim_pkg/build/lib/turtle_sim_pkg/draw_sqaure.py
@@ -7,34 +7,50 @@ class SquarePublisher(Node):
 	def __init__(self):
 		super().__init__('square_publisher')
 		self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel',10)
-		self.timer = self.create_timer(0.5, self.move_square)
-		self.step = 0
-		self.count = 0
+		self.timer = self.create_timer(1.0, self.move_square) # Time para mover a tartaruga
+		# sequencia de comandos: [linear_x, angular_z]
+		self.commands = [
+			(2.0,0.0), # andar para frente
+			(0.0, 1.57), # Girar 90 graus
+			(2.0,0.0),
+			(0.0,1.57),
+			(2.0,0.0),
+			(0.0,1.57),
+			(2.0,0.0),
+			(0.0,1.57),
+			(0.0,0.0) # parar
+		]
+		self.step_index = 0
+	
 		
 	def move_square(self):
-		msg = Twist()
-		if self.step % 2 == 0: # anda para  frente
-			msg.linear.x = 2.0
-			msg.angular.z = 0.0
-			self.count += 1
-		else: # gira 90 graus
-			msg.linear.x = 0.0
-			msg.angular.z = 1.57
-			self.count += 1
+		if self.step_index < len(self.commands):
+			linear_x, angular_z = self.commands[self.step_index]
 			
-		self.publisher_.publish(msg)
-		
-		# alterna passos: frente (x segundos)/gira (alguns segundos)
-		if self.count > 4:
-			self.count = 0
-			self.step +=1
+			msg = Twist()
+			msg.linear.x = linear_x
+			msg.angular.z = angular_z
+			self.publisher_.publish(msg)
+			
+			self.get_logger().info(f'Passo{self.step_index}: Publicando comando linear.x={linear_x:.2f}, angular.z={angular_z:.2f}')
+			
+			self.step_index += 1
+		else:
+			self.get_logger().info('Quadrado completo. Parando o no')
+			# Para encerrar o no
+			self.timer.cancel()
+			self.destroy_node()
+			
 			
 	def main(args=None):
 		rclpy.init(args=args)
 		node = SquarePublisher()
-		rclpy.spin(node)
-		node.destroy_node()
-		rclpy.shutdown()
+		try:
+			rclpy.spin(node)
+		except SystemExit:
+			rclpy.get_logger('square_publisher').info('No encerrado')
+		finally:
+			rclpy.shutdown()
 		
-	if __mname__ == '__name__':
+	if __name__ == '__main__':
 		main()
--- build/turtle_sim_pkg/build/lib/turtle_sim_pkg/go_to_goal.py
+++ build/turtle_sim_pkg/build/lib/turtle_sim_pkg/go_to_goal.py
@@ -3,16 +3,19 @@ from rclpy import Node
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import math
+import sys
 
 class GoToGoal(Node):
-	def __init__(self):
+	def __init__(self, goal_x, goal_y):
 		super().__init__('go_to_goal')
 		self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel',10)
-		self.subscription = self.create_subscription(Pose, 'turtle1/pose', self.pose_callback,10)
+		self.subscription = self.create_subscription(Pose, '/turtle1/pose', self.pose_callback,10)
 		
 		self.pose = Pose()
-		self.goal_x = 5.0 # posicao alvo x
-		self.goal_y = 5.0 # posicao alvo y
+		self.goal_x = goal_x
+		self.goal_y = goal_y
+		
+		self.get_logger().info(f'No GoToGoal iniciado. Indo para a posicao ({self.goal_x:.2f}, {self.goal_y:.2f}).')
 		
 	def pose_callback(self, msg):
 		self.pose = msg
@@ -25,19 +28,35 @@ class GoToGoal(Node):
 		if distance > 0.1: # se ainda nao chegou
 			angle_to_goal = math.atan2(self.goal_y - self.pose_y, self.goal_x - self.pose.x)
 			msg.linear.x = 1.5 * distance
-			msg.angular.z = 4.0 * (angles_to_goal - self.pose.theta)
+			msg.angular.z = 4.0 * (angle_to_goal - self.pose.theta)
 		else:
 			msg.linear.x = 0.0
 			msg.angular.z = 0.0
+			
+			self.get_logger().info(f'Objetivo atingido! Posicao final: ({self.pose.x:.2f}, {self.pose.y:.2f}).')
+			self.destroy_node()
+			return
 			
 		self.publisher_.publish(msg)
 		
 	def main(args=None):
 		rclpy.init(args=args)
-		node = GoToGoal()
+		#verifica se os argumentos foram passados
+		if len(sys.argv) != 3:
+			print("Uso: ros2 run <nome_do_pacote> go_to_goal <x> <y>")
+			rclpy.shutdown()
+			sys.exit(1)
+		try:
+			goal_x = float(sys.argv[1])
+			goal_y = float(sys.argv[2])
+		except:
+			print("Erro, as coordenadas devem ser numeros")
+			rclpy.shutdown()
+			sys.exit(1)
+			
+		node = GoToGoal(goal_x, goal_y)
 		rclpy.spin(node)
-		node.destroy_node()
 		rclpy.shutdown()
 		
-	if __name__ == '__name__':
+	if __name__ == '__main__':
 		main()
